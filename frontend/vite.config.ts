00c7a07836180eedf53b089c16f22f9050a0a439
Isac Petinate <idpetinate@gmail.com>
2026-07-09 12:34:17 -0300

    feat: update deps and bump version + remove tsconfigpaths to use vite self feature

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-import babel from "@rolldown/plugin-babel";
-import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    tsconfigPaths(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
-});
+  resolve: {
+    tsconfigPaths: true,
+  },
+})
